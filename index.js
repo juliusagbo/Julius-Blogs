9a09ab75352b820b129d509e7643f1d062cfebcc
juliusagbo <128760135+juliusagbo@users.noreply.github.com>
2023-12-06 21:12:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function toggleLike(postId) {
+    var likeButton = document.getElementById('likeButton');
+    likeButton.classList.toggle('liked');
+}
+
+function shareContent() {
+    // Get the content you want to share (replace this with your actual content)
+    var contentToShare = "https://restaurantguru.com/Parola-Seaview-Restaurant-Cordova";
+
+    // Use the Clipboard API to copy the content to the clipboard
+    navigator.clipboard.writeText(contentToShare)
+        .then(function() {
+            // Clipboard write successful, perform sharing action
+            alert('Content copied to clipboard. Now you can perform the sharing action.');
+            // Replace the following line with your actual sharing logic, e.g., open a new tab with a share URL
+            // window.open('your_share_url', '_blank');
+        })
+        .catch(function(err) {
+            // Handle errors
+            console.error('Unable to copy to clipboard', err);
+        });
+}
+function addComment() {
+    const commentInput = document.getElementById('commentInput');
+    const commentsList = document.getElementById('commentsList');
+    const newComment = document.createElement('li');
+    newComment.textContent = commentInput.value;
+    commentsList.appendChild(newComment);
+    commentInput.value = ''; // Clear the input after adding a comment
+  }
+
